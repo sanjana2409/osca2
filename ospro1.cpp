2b27acd7905261741fc148438ce609a1a2f7aee0
sanjana2409 <49068966+sanjana2409@users.noreply.github.com>
2019-03-31 15:55:38 +0530

    Add files via upload

---
+++ ospro1.cpp
@@ -0,0 +1,11 @@
+nt main()
+{
+	int p=5,i,j,c=0,x=0,available=332,flag[10],sequence[10],need[10];
+	int allocation[5]={10,200,302,211,002};
+	int max[5]={753,322,902,222,433}; 
+	for(i=0;i<p;i++)
+	{
+	for(i=0;i<p;i++)
+	{
+    	need[i]=max[i]-allocation[i];
+	}}
